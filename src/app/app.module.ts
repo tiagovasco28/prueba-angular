86834de3918569329665ff53e22810d7dd5ef21c
Tiago <tiagoalvesvasco@gmail.com>
2024-01-13 00:11:30 +0100

    ngx-stars library installation and configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgxStarsModule } from 'ngx-stars';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
+    NgxStarsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
