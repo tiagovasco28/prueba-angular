5a189525ae72badc3a01b23c1d474d4f58b46e25
Tiago <tiagoalvesvasco@gmail.com>
2024-01-12 22:05:30 +0100

    Component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    DetailComponent,
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
